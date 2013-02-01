1bb6310ad007af57c032e12eb71c3c79b091c8da
Cirpang <dilafadila48@yahoo.com>
2013-02-01 09:59:25 +0700

    sasa

---
+++ login.php
@@ -0,0 +1,431 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang="en-US" xmlns="http://www.w3.org/1999/xhtml" dir="ltr">
+<head>
+	<title>Indosat Mega Media</title>
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+	<link rel="shortcut icon" href="css/images/favicon.ico" />
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+	<!--[if IE 6]>
+		<link rel="stylesheet" href="css/ie6.css" type="text/css" media="media" />
+		<script src="js/png-fix.js" type="text/javascript" charset="utf-8"></script>
+	<![endif]-->
+	<script src="js/jquery-1.6.2.min.js" type="text/javascript" charset="utf-8"></script>
+	<script src="js/jquery.jcarousel.min.js" type="text/javascript" charset="utf-8"></script>
+	<script src="js/jquery-func.js" type="text/javascript" charset="utf-8"></script>	
+</head>
+<body>
+	<!-- Wrapper -->
+	<div id="wrapper">
+		<!-- Header -->
+		<div id="top">
+			<!-- Shell -->
+			<div class="shell">
+				<div class="top-nav">
+					<ul>
+					    <li class="first nobg"><a href="#" title="Default welcome msg!">Default welcome msg!</a></li>
+					    <li><a href="#" title="Login">Login</a></li>
+					    <li><a href="#" title="My Account">My Account</a></li>
+					    <li><a href="#" title="My Wishlist">My Wishlist</a></li>
+					    <li class="nobg"><a href="#" class="bag" title="My Bag">My Bag</a></li>
+					</ul>
+				</div>
+				<div id="search">
+					<form action="" method="post">
+						<input type="text" class="field" value="Quick search..." title="Quick search..." />
+					</form>
+				</div>
+				<div class="cl">&nbsp;</div>
+			</div>
+			<!-- End Shell -->
+		</div>
+		<!-- End Top -->
+		<!-- Main -->
+		<div id="main">
+			<!-- Shell -->
+			<div class="shell">
+				<!-- Header -->
+				<div id="header">
+					<h1 id="logo"><a href="#" class="notext" title="Shopper Friend">Login From</a></h1>
+					<a><img src="css/images/judul.png" id="cot"/></a>
+					<div id="navigation">
+						<ul>
+						    <li><a href="#" class="active" title="For Boys"><span>For Boys</span></a></li>
+						    <li><a href="#" title="For Girls"><span>For Girls</span></a></li>
+						    <li><a href="#" title="Common"><span>Common</span></a></li>
+						</ul>
+					</div>
+					<div class="cl">&nbsp;</div>
+				</div>
+				<!-- End Header -->
+				<!-- Slider -->
+				<div id="main-slider">
+					<div id="slider-holder">
+						<div class="judul-kiri">
+							<font face="Pristina" size="10" color="black">Member</font>
+						</div>
+						<div class="con-kiri">
+							<table border="0">
+								<tr>
+									<td style="width: 100px;">
+										<font face="Gill Sans MT Condensed" size="5" color="black" >
+											Email
+										</font>
+									</td>
+									<td style="width: 300px;">
+										<input type="text" name="email" placeholder="Email" class="inputlogin">
+									</td>
+								</tr>
+								<tr>
+									<td style="width: 60px;">
+										<font face="Gill Sans MT Condensed" size="5" color="black" colspan="2">
+											Password
+										</font>
+									</td>
+									<td style="width: 60px;">
+										<input type="password" name="password" placeholder="Password" class="inputlogin" colspan="2">
+									</td>
+								</tr>
+								<tr>
+									<td style="width: 60px;" colspan="2">
+										<img src="css/images/button_login.png" style="height: 40px; margin-left: 100px;">
+									</td>
+									
+								</tr>
+							</table>
+						</div>
+						<div class="garis">
+						&nbsp;
+						</div>
+						<div class="judul-kanan">
+							<font face="Pristina" size="10" color="black">New Member</font>
+						</div>
+						<div class="con-kanan">
+							<table border="0">
+								<tr>
+									<td style="width: 180px;">
+										<font face="Gill Sans MT Condensed" size="5" color="black">
+											Username
+										</font>
+									</td>
+									<td style="width: 300px;">
+										<input type="text" name="f_name" placeholder="First Name" style="width: 100px; height: 30px;">
+										<input type="text" name="l_name" placeholder="Last Name" style="width: 173px; height: 30px;">
+									</td>
+								</tr>
+								<tr>
+									<td style="width: 180px;">
+										<font face="Gill Sans MT Condensed" size="5" color="black">
+											Tempat, Tanggal Lahir
+										</font>
+									</td>
+									<td style="width: 300px;">
+										<input type="text" name="birthday_place" placeholder="Birthday_place" style="width: 130px; height: 30px;">
+										<input type="date" name="birthday" style="height: 30px; width: 143px;">
+									</td>
+								</tr>
+								<tr>
+									<td style="width: 180px;">
+										<font face="Gill Sans MT Condensed" size="5" color="black">
+											Jenis Kelamin
+										</font>
+									</td>
+									<td style="width: 300px;">
+										<select class="inputlogin" name="gender" style="width: 284px;">
+											<option selected="selected">Gender</option>
+											<option value="l">Male</option>
+											<option value="p">Female</option>
+										</select>
+									</td>
+								</tr>
+								<tr>
+									<td style="width: 180px;">
+										<font face="Gill Sans MT Condensed" size="5" color="black">
+											No. Telepon
+										</font>
+									</td>
+									<td style="width: 300px;">
+										<input type="text" name="telp" placeholder="Phone Number" class="inputlogin">
+									</td>
+								</tr>
+								<tr>
+									<td style="width: 180px;">
+										<font face="Gill Sans MT Condensed" size="5" color="black">
+											Alamat
+										</font>
+									</td>
+									<td style="width: 300px;">
+										<input type="text" name="addres" placeholder="Addres" class="inputlogin">
+									</td>
+								</tr>
+								<tr>
+									<td style="width: 180px;">
+										<font face="Gill Sans MT Condensed" size="5" color="black">
+											Email
+										</font>
+									</td>
+									<td style="width: 300px;">
+										<input type="text" name="email" placeholder="Email" class="inputlogin">
+									</td>
+								</tr>
+								<tr>
+									<td style="width: 180px;">
+										<font face="Gill Sans MT Condensed" size="5" color="black">
+											Password
+										</font>
+									</td>
+									<td style="width: 300px;">
+										<input type="password" name="password" placeholder="Password" class="inputlogin">
+									</td>
+								</tr>
+								<tr>
+									<td></td>
+									<td style="width: 180px;">
+										<img src="css/images/button_signup.png" style="height: 40px;">
+										<img src="css/images/button_reset.png" style="height: 40px;">
+									</td>
+								</tr>
+							</table>
+						</div>
+					</div>
+					<div class="nav">
+						<a href="#" title="First Slide">&nbsp;</a>
+						<a href="#" title="Second Slide">&nbsp;</a>
+						<a href="#" title="Third Slide">&nbsp;</a>
+					</div>
+				</div>
+				<!-- End Slider -->
+				<!-- Content -->
+				<div id="content">
+					<!-- Case -->
+					<div class="case">
+						<h3>newest</h3>
+						<!-- Row -->
+						<div class="row">
+							<ul>
+							    <li>
+									<a href="#" class="product" title="Product 1">
+										<img src="css/images/product-1.jpg" alt="Product Image 1" />
+										<span class="order model">Model Name</span>
+										<span class="order">catalog number: <span class="number">1925</span></span>
+										<span class="order"><span class="buy-text">Buy Now</span><span class="price"><span class="dollar">$</span>599<span class="sub-text">.99</span></span></span>
+									</a>
+							    </li>
+							    <li>
+									<a href="#" class="product" title="Product 2">
+										<img src="css/images/product-2.jpg" alt="Product Image 2" />
+										<span class="order model">Model Name</span>
+										<span class="order">catalog number: <span class="number">1357</span></span>
+										<span class="order"><span class="buy-text">Buy Now</span><span class="price"><span class="dollar">$</span>1999<span class="sub-text">.99</span></span></span>
+									</a>	
+							    </li>
+							    <li>
+									<a href="#" class="product" title="Product 3">
+										<img src="css/images/product-3.jpg" alt="Product Image 3" />
+										<span class="order model">Model Name</span>
+										<span class="order">catalog number: <span class="number">1264</span></span>
+										<span class="order"><span class="buy-text">Buy Now</span><span class="price"><span class="dollar">$</span>15<span class="sub-text">.99</span></span></span>
+									</a>	
+							    </li>
+							    <li>
+									<a href="#" class="product" title="Product 4">
+										<img src="css/images/product-4.jpg" alt="Product Image 4" />
+										<span class="order model">Model Name</span>
+										<span class="order">catalog number: <span class="number">1111</span></span>
+										<span class="order"><span class="buy-text">Buy Now</span><span class="price"><span class="dollar">$</span>1999<span class="sub-text">.99</span></span></span>
+									</a>	
+							    </li>
+							</ul>
+							<div class="cl">&nbsp;</div>
+						</div>
+						<!-- End Row -->
+						<!-- Row -->
+						<div class="row last-row">
+							<ul>
+							    <li>
+									<a href="#" class="product" title="Product 5">
+										<img src="css/images/product-5.jpg" alt="Product Image 5" />
+										<span class="order model">Model Name</span>
+										<span class="order">catalog number: <span class="number">357</span></span>
+										<span class="order"><span class="buy-text">Buy Now</span><span class="price"><span class="dollar">$</span>874<span class="sub-text">.99</span></span></span>
+									</a>
+							    </li>
+							    <li>
+									<a href="#" class="product" title="Product 6">
+										<img src="css/images/product-6.jpg" alt="Product Image 6" />
+										<span class="order model">Model Name</span>
+										<span class="order">catalog number: <span class="number">1926</span></span>
+										<span class="order"><span class="buy-text">Buy Now</span><span class="price"><span class="dollar">$</span>1199<span class="sub-text">.99</span></span></span>
+									</a>	
+							    </li>
+							    <li>
+									<a href="#" class="product" title="Product 7">
+										<img src="css/images/product-7.jpg" alt="Product Image 7" />
+										<span class="order model">Model Name</span>
+										<span class="order">catalog number: <span class="number">1321</span></span>
+										<span class="order"><span class="buy-text">Buy Now</span><span class="price"><span class="dollar">$</span>27<span class="sub-text">.99</span></span></span>
+									</a>	
+							    </li>
+							    <li>
+									<a href="#" class="product" title="Product 8">
+										<img src="css/images/product-8.jpg" alt="Product Image 8" />
+										<span class="order model">Model Name</span>
+										<span class="order">catalog number: <span class="number">299</span></span>
+										<span class="order"><span class="buy-text">Buy Now</span><span class="price"><span class="dollar">$</span>4<span class="sub-text">.99</span></span></span>
+									</a>	
+							    </li>
+							</ul>
+							<div class="cl">&nbsp;</div>
+						</div>
+						<!-- End Row -->
+					</div>
+					<!-- End Case -->
+					<!-- Case -->
+					<div class="case">
+						<h3>Bestsellers</h3>
+						<!-- Products Slider -->
+						<div class="products-slider">
+							<div class="slider-holder">
+								<ul>
+								    <li>
+								    	<a class="product" title="Product 9">
+											<img src="css/images/product-9.jpg" alt="Product Image 9" />
+											<span class="order model">Model Name</span>
+										<span class="order">catalog number: <span class="number">347</span></span>
+											<span class="order"><span class="buy-text">Buy Now</span><span class="price"><span class="dollar">$</span>14<span class="sub-text">.99</span></span></span>
+										</a>
+								    </li>
+								    <li>
+								    	<a class="product" title="Product 10">
+											<img src="css/images/product-10.jpg" alt="Product Image 10" />
+											<span class="order model">Model Name</span>
+										<span class="order">catalog number: <span class="number">537</span></span>
+											<span class="order"><span class="buy-text">Buy Now</span><span class="price"><span class="dollar">$</span>24<span class="sub-text">.99</span></span></span>
+										</a>
+								    </li>
+								    <li>
+								    	<a class="product" title="Product 11">
+											<img src="css/images/product-11.jpg" alt="Product Image 11" />
+											<span class="order model">Model Name</span>
+											<span class="order">catalog number: <span class="number">710</span></span>
+											<span class="order"><span class="buy-text">Buy Now</span><span class="price"><span class="dollar">$</span>4<span class="sub-text">.99</span></span></span>
+										</a>
+								    </li>
+								    <li>
+								    	<a class="product" title="Product 12">
+											<img src="css/images/product-12.jpg" alt="Product Image 12" />
+											<span class="order model">Model Name</span>
+											<span class="order">catalog number: <span class="number">32</span></span>
+											<span class="order"><span class="buy-text">Buy Now</span><span class="price"><span class="dollar">$</span>7<span class="sub-text">.99</span></span></span>
+										</a>
+								    </li>
+								     <li>
+								    	<a class="product" title="Product 9">
+											<img src="css/images/product-9.jpg" alt="Product Image 9" />
+											<span class="order model">Model Name</span>
+										<span class="order">catalog number: <span class="number">347</span></span>
+											<span class="order"><span class="buy-text">Buy Now</span><span class="price"><span class="dollar">$</span>14<span class="sub-text">.99</span></span></span>
+										</a>
+								    </li>
+								    <li>
+								    	<a class="product" title="Product 10">
+											<img src="css/images/product-10.jpg" alt="Product Image 10" />
+											<span class="order model">Model Name</span>
+										<span class="order">catalog number: <span class="number">537</span></span>
+											<span class="order"><span class="buy-text">Buy Now</span><span class="price"><span class="dollar">$</span>24<span class="sub-text">.99</span></span></span>
+										</a>
+								    </li>
+								    <li>
+								    	<a class="product" title="Product 11">
+											<img src="css/images/product-11.jpg" alt="Product Image 11" />
+											<span class="order model">Model Name</span>
+											<span class="order">catalog number: <span class="number">710</span></span>
+											<span class="order"><span class="buy-text">Buy Now</span><span class="price"><span class="dollar">$</span>4<span class="sub-text">.99</span></span></span>
+										</a>
+								    </li>
+								    <li>
+								    	<a class="product" title="Product 12">
+											<img src="css/images/product-12.jpg" alt="Product Image 12" />
+											<span class="order model">Model Name</span>
+											<span class="order">catalog number: <span class="number">32</span></span>
+											<span class="order"><span class="buy-text">Buy Now</span><span class="price"><span class="dollar">$</span>7<span class="sub-text">.99</span></span></span>
+										</a>
+								    </li>
+								</ul>
+							</div>
+							<div class="nav">
+								<a href="#" class="prev" title="Previous">Prev</a>
+								<a href="#" class="next" title="Next">Next</a>
+								<div class="cl">&nbsp;</div>
+							</div>
+						</div>
+						<!-- End Products Slider -->
+					</div>
+					<!-- End Case -->
+				</div>
+				<!-- End Content -->
+			</div>
+			<!-- End Shell -->
+		</div>
+		<!-- End Main -->
+		<div id="footer-push" class="cl">&nbsp;</div>
+	</div>
+	<!-- End Wrapper -->
+	<!-- Footer -->
+	<div id="footer">
+		<!-- Shell -->
+		<div class="shell">
+			<!-- Cols -->
+			<div class="cols">
+				<ul>
+				    <li class="col">
+				    	<h4>about wooden toy</h4>
+				    	<p>Quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. <a href="#" title="Duis autem">Duis autem</a> vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu</p>
+				    	<a href="#" class="more-link" title="Read More">Read More >></a>
+				    </li>
+				    <li class="col social">
+				    	<h4>get social</h4>
+				    	<ul>
+				    	    <li><a href="#" class="fb-link" title="Facebook">facebook</a></li>
+				    	    <li><a href="#" class="twitter-link" title="Twitter">twitter</a></li>
+				    	    <li><a href="#" class="behance-link" title="Behance">behance</a></li>
+				    	    <li><a href="#" class="blogger-link" title="Blogger">blogger</a></li>
+				    	    <li><a href="#" class="digg-link" title="Digg">digg</a></li>
+				    	</ul>
+				    </li>
+				    <li class="col partners">
+				    	<h4>partners</h4>
+				    	<ul>
+				    	    <li><a href="#" title="Lorem ipsum dolor">Lorem ipsum dolor</a></li>
+				    	    <li><a href="#" title="Cras eu lorem id mauris">Cras eu lorem id mauris</a></li>
+				    	    <li><a href="#" title="Proin et velit ut libero">Proin et velit ut libero</a></li>
+				    	    <li><a href="#" title="Velit esse molestie consequat">Velit esse molestie consequat</a></li>
+				    	    <li><a href="#" title="Vel illum dolore eu">Vel illum dolore eu</a></li>
+				    	</ul>
+				    </li>
+				    <li class="col contact">
+				    	<h4>newsletter</h4>
+				    	<p>Etiam consectetur dui dignissim nulla</p>
+				    	<form action="" method="post">
+				    		<div class="field-wrapper">
+				    			<input type="text" class="field" value="Name" title="Name" />
+				    		</div>
+				    		<div class="field-wrapper">
+				    			<input type="text" class="field" value="Email" title="Email" />
+				    		</div>
+				    		<input type="submit" value="Submit" class="submit-btn" title="Submit" />
+				    		<div class="cl">&nbsp;</div>
+				    	</form>
+				    </li>
+				</ul>
+				<div class="cl">&nbsp;</div>
+			</div>
+			<!-- End Cols -->
+			<p class="copy">&copy; Sitename.com. Design by <a href="http://css-free-templates.com/">CSS-FREE-TEMPLATES.COM</a></p>
+			<a href="#" class="logo" title="Shopper Friend"><img src="css/images/footer-logo.png" alt="Shopper Friend" /></a>
+			<div class="cl">&nbsp;</div>
+		</div>
+		<!-- End Shell -->
+	</div>
+	<!-- End Footer -->
+</body>
+</html>
