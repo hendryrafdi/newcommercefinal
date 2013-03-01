4709fa6153d54d7fb0419275d356e914248bd7ef
Hendry Abi Rafdi <hendry.rafdi@live.com>
2013-03-01 09:11:35 +0700

    db

--- db_commerce.sql
+++ db_commerce.sql
@@ -35,7 +35,7 @@ CREATE TABLE `category` (
 
 LOCK TABLES `category` WRITE;
 /*!40000 ALTER TABLE `category` DISABLE KEYS */;
-INSERT INTO `category` VALUES (1,'computer'),(2,'fashion'),(3,'gadget');
+INSERT INTO `category` VALUES (1,'Computer'),(2,'Fashion'),(3,'Gadget');
 /*!40000 ALTER TABLE `category` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -94,7 +94,7 @@ CREATE TABLE `orders` (
   KEY `id_product_2` (`id_product`),
   KEY `id_product_3` (`id_product`),
   CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -103,6 +103,7 @@ CREATE TABLE `orders` (
 
 LOCK TABLES `orders` WRITE;
 /*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+INSERT INTO `orders` VALUES (1,1,2,'Hendry','Software Transformator','Jl. Kutilang 4 No.41','Depok',16432,'Jawa Barat','Indonesia',2147483647,1,'cod','2013-02-05 17:00:24',NULL),(2,1,4,'Hendry Rafdi','Software Transformator','Jl. Kutilang 4 No.41','Depok',16432,'Jawa Barat','Indonesia',2147483647,1,'cod','2013-02-05 17:00:24',NULL),(3,1,1,'Hendry Rafdi','Software Transformator','Jl. Kutilang 4 No.41','Depok',16432,'Jawa Barat','Indonesia',2147483647,1,'cod','2013-02-05 17:00:24',NULL),(4,7,5,'Frets Eric','Capricornboy','Jl. Raya Bogor','Jakarta Timur',13425,'DKI Jakarta','Indonesia',901920901,1,'cod','2013-02-05 17:00:24',NULL),(5,9,1,'alsolendski prakoso','','Jl. Raya Bogor','Depok',16432,'Jawa Barat','Indonesia',818898983,1,'cod','2013-02-07 17:00:24',NULL),(6,9,3,'alsolendski prakoso','','Jl. Raya Bogor','Depok',16432,'Jawa Barat','Indonesia',818898983,1,'cod','2013-02-07 17:00:24',NULL);
 /*!40000 ALTER TABLE `orders` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -154,7 +155,7 @@ CREATE TABLE `shoppingcart` (
   PRIMARY KEY (`id_shopping`),
   KEY `id_product` (`id_product`),
   CONSTRAINT `shoppingcart_ibfk_1` FOREIGN KEY (`id_product`) REFERENCES `product` (`id_product`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -164,6 +165,31 @@ CREATE TABLE `shoppingcart` (
 LOCK TABLES `shoppingcart` WRITE;
 /*!40000 ALTER TABLE `shoppingcart` DISABLE KEYS */;
 /*!40000 ALTER TABLE `shoppingcart` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `slideshow`
+--
+
+DROP TABLE IF EXISTS `slideshow`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `slideshow` (
+  `id_slide` int(11) NOT NULL AUTO_INCREMENT,
+  `nm_slide` varchar(30) NOT NULL,
+  `link` text NOT NULL,
+  PRIMARY KEY (`id_slide`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `slideshow`
+--
+
+LOCK TABLES `slideshow` WRITE;
+/*!40000 ALTER TABLE `slideshow` DISABLE KEYS */;
+INSERT INTO `slideshow` VALUES (1,'Apple Macbook Air','images/slideshow/Apple Macbook Air.jpg'),(2,'Iphone','images/slideshow/iphone.jpg'),(3,'Iphone5','images/slideshow/iphone5.jpg'),(4,'iphone-5','images/slideshow/iphone-5.jpg');
+/*!40000 ALTER TABLE `slideshow` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -184,9 +210,10 @@ CREATE TABLE `user` (
   `telp` int(12) NOT NULL,
   `email` varchar(40) NOT NULL,
   `password` varchar(20) NOT NULL,
+  `level` varchar(1) NOT NULL,
   PRIMARY KEY (`id_user`,`email`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -195,7 +222,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'Hendry','Rafdi','l','Depok','1996-01-25','',12321930,'hendry.rafdi@live.com','amoeba123'),(2,'Rizqi','Fadilla','l','jakarta','1996-10-22','Condet City',901920901,'dilafadila48@yahoo.com','bebas'),(3,'Depok','City','l','Depok','1999-01-02','Depok',1293123,'depok@city.co.id','depok'),(6,'Momo','Pedro','p','Jakarta','2013-02-04','Jakarta',901920901,'momo@gmail.com','momo'),(7,'Ambon','Eric','l','Ambon','1995-12-01','Jakarta',19023123,'ambon@capricornboy.tk','123456'),(8,'Geisha','','p','jakarta','1991-01-01','Jakarta',2109312,'geisha@momo.com','123');
+INSERT INTO `user` VALUES (1,'Hendry','Rafdi','l','Depok','1996-01-25','',12321930,'hendry.rafdi@live.com','amoeba123','a'),(2,'Rizqi','Fadilla','l','jakarta','1996-10-22','Condet City',901920901,'dilafadila48@yahoo.com','bebas','a'),(3,'Depok','City','l','Depok','1999-01-02','Depok',1293123,'depok@city.co.id','depok','u'),(6,'Momo','Pedro','p','Jakarta','2013-02-04','Jakarta',901920901,'momo@gmail.com','momo','u'),(7,'Ambon','Eric','l','Ambon','1995-12-01','Jakarta',19023123,'ambon@capricornboy.tk','123456','a'),(8,'Geisha','','p','jakarta','1991-01-01','Jakarta',2109312,'geisha@momo.com','123','u'),(9,'alsolendski','prakoso','l','jakarta','2004-02-03','lfalkfjdklfj',0,'alsolendski@gmail.com','123123','u');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -208,4 +235,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2013-02-05  9:09:50
+-- Dump completed on 2013-03-01  9:09:38
