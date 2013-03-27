49f63bbe5448a568fd732e4bf72165e565a594d6
Cirpang <Cirpengcirpang@yahoo.co.id>
2013-03-27 11:16:59 +0700

    image

--- pages/logout.php
+++ pages/logout.php
@@ -1,6 +1,11 @@
 <?php
+	include 'config.php';
   session_start();
   session_destroy();
+  
+  $sql = mysql_query("truncate table shoppingcart");
+  
   echo "<script>window.alert('Kamu telah berhasil keluar dari sistem administrator');
         window.location=('../index.php')</script>";
+	
 ?>
